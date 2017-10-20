c95a6bd90b7dde8ef5cfc783436af50c255e82cb
teimurjan <teymurgg321@gmail.com>
2017-10-20 21:58:37 +0600

    Added Registration folder and route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Route} from "react-router";
 import App from "./app/AppContainer";
+import Registration from "./registration/RegistrationContainer";
 
 export default (
   <div>
-    <Route path='/' component={App}/>
+    <Route path='/' component={App}>
+      <Route path='registration' component={Registration}/>
+    </Route>
   </div>
 )
