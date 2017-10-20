33bd8d768a51d9f616618cc4618783219c4084cf
Alexey Buzlov <intern@zensoft.io>
2017-10-21 02:47:23 +0600

    registration page

--- src/registration/Registration.js
+++ src/registration/Registration.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {InputField} from "../common/input-field/InputField";
 import './Registration.scss';
 import {Button} from "../common/button/Button";
+import img from "../assets/images/lightning.svg";
 
 export default class Registration extends React.Component {
   static propTypes = {
@@ -21,9 +22,11 @@ export default class Registration extends React.Component {
     return (
       <div id='registration'>
         <div className="wrapper">
-          <InputField label='E-mail' inputProps={{onChange: this.handleNameChange, type: 'email'}}/>
+          <img src={img} className='title-icon'/>
+          <div className='title'>Регистрация</div>
+          <InputField className='input-signup' label='E-mail' inputProps={{onChange: this.handleNameChange, type: 'email'}}/>
           <InputField label='Пароль' inputProps={{onChange: this.handlePasswordChange, type: 'password'}}/>
-          <Button disabled={this.props.isLoading} onClick={this.props.actions.submit}>Зарегистрироваться</Button>
+          <Button className='primary-button signup-button' disabled={this.props.isLoading} onClick={this.props.actions.submit}>Зарегистрироваться</Button>
         </div>
       </div>
     );
