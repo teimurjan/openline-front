2570a6db2aea7219d087d5782cf30d2bf67cca53
Alexey Buzlov <intern@zensoft.io>
2017-10-21 16:29:30 +0600

    Footer

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import './App.scss';
 import PropTypes from 'prop-types';
-import Header from '../common/header/Header.js'
+import Header from '../common/header/Header.js';
+import Footer from '../common/footer/Footer.js';
 
 export default class App extends Component {
   static propTypes = {
@@ -13,6 +14,7 @@ export default class App extends Component {
       <div id='app'>
         <Header></Header>
         {this.props.children}
+        <Footer></Footer>
       </div>
     );
   }
---
+++ src/common/footer/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../assets/images/true-hero.svg';
+// import moment from 'moment';
+// import LoadingLine from '../loading-line/LoadingLine';
+// import UserLine from '../user-line/UserLine';
+import Button from '../button/Button';
+import {Link} from "react-router";
+import './Footer.scss';
+
+export default class Header extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+     <div className='footer-container'>
+         <div className='content'>
+            <div className='column'>
+                <a className='link title'>О НАС</a>
+                <a className='link'>Кто мы?</a>
+                <a className='link'>Фаилы</a>
+                <a className='link'>Спецпроекты</a>
+                <a className='link'>Партнеры</a>
+            </div>
+             <div className='column'>
+                 <a className='link title'>СПОНСОРАМ</a>
+                 <a className='link'>Все проекты</a>
+                 <a className='link'>Новые проекты</a>
+                 <a className='link'>Способы оплаты</a>
+                 <a className='link'>Возврат средств</a>
+             </div>
+             <div className='column'>
+                 <a className='link'>Контакты</a>
+                 <a className='link'>Правила сервиса</a>
+                 <a className='link'>Задать вопрос</a><br/>
+                 <img src={logo}/>
+             </div>
+         </div>
+     </div>
+    )
+  }
+};
+
+// Card.propTypes = {
+//   project: PropTypes.object,
+//   user: PropTypes.object,
+// };
