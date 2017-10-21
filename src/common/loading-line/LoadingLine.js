fb6c3cdba9a5b9a8068f93ce25fe4aca3a5c1db7
Andrew <hegai.andrei@gmail.com>
2017-10-21 12:46:50 +0600

    - loading line customization  - wide card

--- src/common/loading-line/LoadingLine.js
+++ src/common/loading-line/LoadingLine.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './LoadingLine.scss'
 
-const LoadingLine = ({percents, backgroundColor='#24DB71'}) => (
-  <div className="loading-line" >
-    <div className="bar" style={{width: `${percents}%`, backgroundColor}}/>
+const LoadingLine = ({percents, lineColor='#24DB71', backgroundColor='#ffffff', height='4px'}) => (
+  <div className="loading-line" style={{backgroundColor: backgroundColor, height}}>
+    <div className="bar" style={{width: `${percents}%`, backgroundColor: lineColor}}/>
   </div>
 );
 
